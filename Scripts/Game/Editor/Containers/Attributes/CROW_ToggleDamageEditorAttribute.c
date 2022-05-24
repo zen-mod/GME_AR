058dfc3d4ab4568d38ff3e3295e124680d87c0de
Frederik Mazur Andersen <fm@fmazur.dev>
2022-05-24 20:00:05 +0200

    init

---
+++ Scripts/Game/Editor/Containers/Attributes/CROW_ToggleDamageEditorAttribute.c
@@ -0,0 +1,34 @@
+/**
+Entity Attribute for toggle damage on unit
+*/
+[BaseContainerProps(), SCR_BaseEditorAttributeCustomTitle()]
+class CROW_ToggleDamageEditorAttribute : SCR_BaseEditorAttribute
+{
+	override SCR_BaseEditorAttributeVar ReadVariable(Managed item, SCR_AttributesManagerEditorComponent manager)
+	{
+		
+		SCR_EditableEntityComponent editableEntity = SCR_EditableEntityComponent.Cast(item);
+		if (!editableEntity) return null;
+		
+		IEntity owner =  editableEntity.GetOwner();
+		if (!owner) return null;
+		DamageManagerComponent damageComponent = DamageManagerComponent.Cast(owner.FindComponent(DamageManagerComponent));
+		
+		if (!damageComponent) return null;
+		
+		return SCR_BaseEditorAttributeVar.CreateBool(damageComponent.IsDamageHandlingEnabled());		
+	}
+	override void WriteVariable(Managed item, SCR_BaseEditorAttributeVar var, SCR_AttributesManagerEditorComponent manager, int playerID)
+	{
+		if (!var) return;
+		SCR_EditableEntityComponent editableEntity = SCR_EditableEntityComponent.Cast(item);
+		
+		IEntity owner =  editableEntity.GetOwner();
+		if (!owner) return;
+		DamageManagerComponent damageComponent = DamageManagerComponent.Cast(owner.FindComponent(DamageManagerComponent));
+		if (!damageComponent) return;
+			
+		// toggle damage handling 
+		damageComponent.EnableDamageHandling(var.GetBool());
+	}
+};
