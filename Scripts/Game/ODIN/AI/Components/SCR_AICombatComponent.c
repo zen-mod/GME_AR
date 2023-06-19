99aea20200b6f2be2802335a30c8d18cba8ed57d
Crowdedlight <crowdedhd@gmail.com>
2023-06-19 21:01:58 +0200

    pulled latest from develop with new folder structure and added so AI doesn't target invisible units

---
+++ Scripts/Game/ODIN/AI/Components/SCR_AICombatComponent.c
@@ -0,0 +1,18 @@
+
+
+modded class SCR_AICombatComponent : ScriptComponent
+{
+	override bool SelectTarget(BaseTarget newTarget)
+	{
+		// gotta do null check as otherwise we crash, as when there is no targets at spawn, NewTarget is null. So we just call super and call it a day
+		if (newTarget == null)
+			return super.SelectTarget(newTarget);
+		
+		// get visibility, if not visible AI can't change to that target, otherwise up to super
+		bool visible = ODIN_VisibilityHelper.GetVisibility(newTarget.GetTargetEntity());
+		if (!visible)
+			return false;
+		else
+			return super.SelectTarget(newTarget);
+	}
+};
