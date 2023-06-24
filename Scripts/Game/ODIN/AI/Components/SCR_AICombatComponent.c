8e5a89ec928c17c34502bfdd91d6d71ca169ab92
Crowdedlight <fm@fmazur.dev>
2023-06-24 17:11:50 +0200

    Update Scripts/Game/ODIN/AI/Components/SCR_AICombatComponent.c
    Co-authored-by: Kex <kexanone@gmail.com>

--- Scripts/Game/ODIN/AI/Components/SCR_AICombatComponent.c
+++ Scripts/Game/ODIN/AI/Components/SCR_AICombatComponent.c
@@ -5,7 +5,7 @@ modded class SCR_AICombatComponent : ScriptComponent
 	override bool SelectTarget(BaseTarget newTarget)
 	{
 		// gotta do null check as otherwise we crash, as when there is no targets at spawn, NewTarget is null. So we just call super and call it a day
-		if (newTarget == null)
+		if (!newTarget)
 			return super.SelectTarget(newTarget);
 		
 		// get visibility, if not visible AI can't change to that target, otherwise up to super
