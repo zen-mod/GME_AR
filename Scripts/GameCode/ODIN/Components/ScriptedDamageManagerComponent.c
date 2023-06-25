0c89a8757e13784e839ce878246efddadacaa4f2
Crowdedlight <crowdedhd@gmail.com>
2023-06-26 00:02:59 +0200

    moved character disable damage to own implementation as current one is unreliable

---
+++ Scripts/GameCode/ODIN/Components/ScriptedDamageManagerComponent.c
@@ -0,0 +1,44 @@
+
+//------------------------------------------------------------------------------------------------
+modded class ScriptedDamageManagerComponent : BaseScriptedDamageManagerComponent
+{
+	[RplProp(onRplName: "ODIN_OnEnableDamageValueUpdated")]
+	protected bool m_bODIN_isDamageEnabled = true;
+	
+	bool ODIN_IsDamageEnabled()
+	{
+		return m_bODIN_isDamageEnabled;
+	}
+	
+	void ODIN_SetDamageEnabled(bool enabled)
+	{
+		m_bODIN_isDamageEnabled = enabled;
+		Print(m_bODIN_isDamageEnabled);
+		Replication.BumpMe();
+		this.ODIN_OnEnableDamageValueUpdated();
+	}
+	
+	void ODIN_OnEnableDamageValueUpdated()
+	{
+		Print("value update");
+		// add function to invoker if damage is disabled
+		if (!m_bODIN_isDamageEnabled)
+			this.GetOnDamage().Insert(this.ODIN_disableDamage);
+		else // What happens if we try to remove and it never was added?
+			this.GetOnDamage().Remove(this.ODIN_disableDamage);
+	}
+	
+	void ODIN_disableDamage(EDamageType type, float damage, HitZone pHitZone, IEntity instigator, inout vector outMat[3], float speed, int colliderID, int nodeID)
+	{
+		ChimeraCharacter char = ChimeraCharacter.Cast(this.GetOwner());
+		// null check might not be needed as this lives inside the ScriptedDamageManager component, and as such a char should always be "connected"
+		if (!char)
+			return;
+		// do full heal 
+		SCR_CharacterDamageManagerComponent dmgComponent = SCR_CharacterDamageManagerComponent.Cast(char.FindComponent(SCR_CharacterDamageManagerComponent));		
+		if (!dmgComponent)
+			return;
+		
+		dmgComponent.FullHeal();
+	}	
+};
