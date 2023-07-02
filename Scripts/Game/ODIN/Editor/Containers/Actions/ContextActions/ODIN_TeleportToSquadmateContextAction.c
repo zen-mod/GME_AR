827e3b9da0de9fb5280e265927ded0858dac5889
Crowdedlight <crowdedhd@gmail.com>
2023-07-02 21:41:02 +0200

    fixed listbox layout and added missing scripts for tp2squadmate

--- Scripts/Game/ODIN/Editor/Containers/Actions/ContextActions/ODIN_TeleportToSquadmateContextAction.c
+++ Scripts/Game/ODIN/Editor/Containers/Actions/ContextActions/ODIN_TeleportToSquadmateContextAction.c
@@ -13,8 +13,11 @@ class ODIN_TeleportToSquadmateContextAction: SCR_BaseContextAction
 		
 		if (!SCR_ChimeraCharacter.Cast(owner))
 			return false;
+				
+		// TODO only able to do on players
+		if (!SCR_EntityHelper.IsAPlayer(owner))
+			return false;
 		
-			//IEntity controlledEntity = SCR_PlayerController.GetLocalControlledEntity();
 		return true;
 	}
 	
@@ -39,19 +42,81 @@ class ODIN_TeleportToSquadmateContextAction: SCR_BaseContextAction
 			return;	
 		}
 		
+		// cast so we have dialog object. Set hovered entity, we need to get squadmates from it and have it to teleport it
 		ODIN_TeleportToSquadmateDialog myMenuUI = ODIN_TeleportToSquadmateDialog.Cast(myMenu);
-		myMenuUI.myCallerEntity = owner;
-		// with the method above we can make functions in listbox for setting listbox data, title etc. For generic listbox item?
+		myMenuUI.SetHoveredEntity(hoveredEntity);
+		
+		// set Title 
+		myMenuUI.SetTitle("Teleport To Squadmate");
+			
+		// Get squadmates 
+		SCR_AIGroup group = ODIN_GroupHelper.GetGroup(owner);
+		if (!group || group.GetPlayerCount() <= 0) 
+			return; // TODO give error message back to zeus that unit is not in a group 
+		
+		// can't get character entities directly from group, only for AI. So for players we get their IDs and gotta find and cast them with that
+		array<int> members = group.GetPlayerIDs();
+		foreach (int playerID : members)
+		{
+			PlayerManager playerManager = GetGame().GetPlayerManager();
+			string name = playerManager.GetPlayerName(playerID);
+			ChimeraCharacter player = ChimeraCharacter.Cast(playerManager.GetPlayerControlledEntity(playerID));
+			
+			// don't add the unit we want to teleport
+			if (player.GetID() == owner.GetID())
+				continue;
+			
+			// if Leader, show it
+			if (group.IsPlayerLeader(playerID))
+				name += " (Leader)";
+			
+			// if in Vehicle, show it
+			if (player.IsInVehicle())
+				name += " [VEHICLE]";
+			
+			myMenuUI.AddItemToListbox(name, player);
+		}
+		
+		// add action to confirm 
+		myMenuUI.GetOnConfirm().Insert(ODIN_TeleportToSquadmateContextAction.ODIN_OnConfirm);
+		
+		// TODO remember to move all strings to stringtable!
 		
 		// TODO figure out if we pass hoveredentity, or get squadmates here and populate the dialog? Can we actually make a generic dialog and populate it from here? 
 		// Would have to be able to pass data to it on creation and bind callback to its "ok" method. 
 		
 		//SCR_PlayersManagerEditorComponent playersManager = SCR_PlayersManagerEditorComponent.Cast(SCR_PlayersManagerEditorComponent.GetInstance(SCR_PlayersManagerEditorComponent));
 		//playersManager.TeleportPlayerToPosition(cursorWorldPosition);
-		
 	}
+	
+	// function for script invoker
+	static void ODIN_OnConfirm(SCR_EditableEntityComponent hoveredEntity, SCR_ListBoxComponent listbox)
+	{
+		// todo, get selected player 
+		int selectedItem = listbox.GetSelectedItem();
+		ChimeraCharacter selectedPlayer = ChimeraCharacter.Cast(listbox.GetItemData(selectedItem));
+
+		if (selectedPlayer.IsInVehicle())
+		{
+			// try to put hoveredentity inside vehicle? otherwise keep going and put it next to vic 
+		}
+		
+		// Get world coordinates of player 
+		vector target_pos;
+		SCR_WorldTools.FindEmptyTerrainPosition(target_pos, selectedPlayer.GetOrigin(), 20);
+		
+		// get transform for rotation etc. 
+		vector target_transform[4];
+		hoveredEntity.GetTransform(target_transform);
+		
+		// use new position, but keep rotation 
+		target_transform[3] = target_pos;
+		
+		hoveredEntity.SetTransform(target_transform, true);
+	}
+	
 	override bool IsServer()
 	{
-		return false;
+		return true;
 	}
 };
---
+++ Scripts/Game/ODIN/Helpers/ODIN_GroupHelper.c
@@ -0,0 +1,21 @@
+
+
+class ODIN_GroupHelper
+{
+	static SCR_AIGroup GetGroup(GenericEntity entity)
+	{
+		PlayerManager playerManager = GetGame().GetPlayerManager();
+		
+		if (!playerManager)
+			return null;
+		
+		SCR_GroupsManagerComponent groupManager = SCR_GroupsManagerComponent.Cast(GetGame().GetGameMode().FindComponent(SCR_GroupsManagerComponent));
+		
+		if (!groupManager)
+			return null;
+
+		int playerId = playerManager.GetPlayerIdFromControlledEntity(entity);
+		
+		return groupManager.GetPlayerGroup(playerId);
+	};
+}
