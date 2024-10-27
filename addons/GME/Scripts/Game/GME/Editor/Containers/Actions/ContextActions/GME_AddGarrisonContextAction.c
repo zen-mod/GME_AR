ae341ec1e9c877d8058e53d1d3fab0ddfc674ab3
Kex <kexanone@gmail.com>
2024-10-27 19:57:12 +0100

    Add method for requesting the spawning of an interactive module

--- addons/GME/Scripts/Game/GME/Editor/Containers/Actions/ContextActions/GME_AddGarrisonContextAction.c
+++ addons/GME/Scripts/Game/GME/Editor/Containers/Actions/ContextActions/GME_AddGarrisonContextAction.c
@@ -2,6 +2,9 @@
 [BaseContainerProps(), SCR_BaseContainerCustomTitleUIInfo("m_Info")]
 class GME_AddGarrisonContextAction : SCR_BaseContextAction
 {
+	[Attribute(defvalue: "{0B9374FB0BA34B16}PrefabsEditable/Modules/AI/E_GME_Modules_AddGarrison.et", uiwidget: UIWidgets.ResourcePickerThumbnail, desc: "Prefab name of the module to be spawned", params: "et")]
+	protected ResourceName m_sModulePrefabName;
+	
 	protected SCR_DestructibleBuildingEntity m_pBuilding;
 	
 	//------------------------------------------------------------------------------------------------
@@ -35,20 +38,13 @@ class GME_AddGarrisonContextAction : SCR_BaseContextAction
 	//------------------------------------------------------------------------------------------------
 	override void Perform(SCR_EditableEntityComponent hoveredEntity, notnull set<SCR_EditableEntityComponent> selectedEntities, vector cursorWorldPosition,int flags, int param = -1)
 	{
-		EntitySpawnParams params = new EntitySpawnParams();
-		params.TransformMode = ETransformMode.WORLD;
-		m_pBuilding.GetWorldTransform(params.Transform);
+		vector transform[4];
+		m_pBuilding.GetWorldTransform(transform);
 		
-		GME_Modules_AddGarrison module = GME_Modules_AddGarrison.Cast(GetGame().SpawnEntityPrefab(Resource.Load("{0B9374FB0BA34B16}PrefabsEditable/Modules/AI/E_GME_Modules_AddGarrison.et"), null, params));
-		if (!module)
+		SCR_PlacingEditorComponent placingComponent = SCR_PlacingEditorComponent.Cast(SCR_PlacingEditorComponent.GetInstance(SCR_PlacingEditorComponent));
+		if (!placingComponent)
 			return;
 		
-		module.SetBuilding(m_pBuilding);
-		
-		GME_Modules_EditableModuleComponent editableModuleComponent = GME_Modules_EditableModuleComponent.Cast(SCR_EditableEntityComponent.GetEditableEntity(module));
-		if (!editableModuleComponent)
-			return;
-		
-		editableModuleComponent.OnInitServer();
+		placingComponent.GME_RequestPlaceModule(m_sModulePrefabName, transform);
 	}
 }
