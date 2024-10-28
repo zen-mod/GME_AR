b3fcc09eec76de5d8f26217eef347b6ce84df68d
Kex <kexanone@gmail.com>
2024-10-28 11:27:39 +0100

    Enhance skill attribute (#7)
    * Update skill for SCR_AICombatComponent too
    
    * Split skill attribute to character and group level
    
    * Replace group skill with character skill
    
    * Add global skill attribute
    
    * Update attribute config
    
    * Use setter for applying mission header setting
    
    * Apply default skill if global one is unset

--- addons/GME/Scripts/GameCode/GME/Character/SCR_ChimeraCharacter.c
+++ addons/GME/Scripts/GameCode/GME/Character/SCR_ChimeraCharacter.c
@@ -1,18 +1,34 @@
-
-
+//------------------------------------------------------------------------------------------------
 modded class SCR_ChimeraCharacter : ChimeraCharacter
 {
 	[RplProp(onRplName: "GME_OnVisibilityValueUpdated")]
 	protected bool m_bGME_isVisible = true;
 	
-	void GME_SetVisibility(bool visible)
-	{		
-		m_bGME_isVisible = visible;
-		Replication.BumpMe();
-		this.GME_OnVisibilityValueUpdated();
+	//------------------------------------------------------------------------------------------------
+	//! Apply global AI skill
+	override void EOnInit(IEntity owner)
+	{
+		super.EOnInit(owner);
+		
+		if (!GetGame().InPlayMode() || !Replication.IsServer())
+			return;
+		
+		SCR_BaseGameMode gameMode = SCR_BaseGameMode.Cast(GetGame().GetGameMode());
+		if (!gameMode)
+			return;
+		
+		SCR_AICombatComponent combatComponent = SCR_AICombatComponent.Cast(FindComponent(SCR_AICombatComponent));
+		if (!combatComponent)
+			return;
+		
+		EAISkill skill = gameMode.GME_GetGlobalAISkill();
+		if (skill == EAISkill.NONE)
+			return;
+	
+		combatComponent.SetAISkill(skill);
 	}
 	
-	
+	//------------------------------------------------------------------------------------------------
 	void GME_OnVisibilityValueUpdated()
 	{
 		if (m_bGME_isVisible)
@@ -20,4 +36,12 @@ modded class SCR_ChimeraCharacter : ChimeraCharacter
 		else
 			this.ClearFlags(EntityFlags.VISIBLE|EntityFlags.TRACEABLE);
 	}
-};
+	
+	//------------------------------------------------------------------------------------------------
+	void GME_SetVisibility(bool visible)
+	{		
+		m_bGME_isVisible = visible;
+		Replication.BumpMe();
+		this.GME_OnVisibilityValueUpdated();
+	}
+}
