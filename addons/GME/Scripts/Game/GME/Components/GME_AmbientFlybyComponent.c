0a502972c7ddddd74a94cb08068d332ea2758f46
Kex <kexanone@gmail.com>
2024-10-28 11:38:08 +0100

    Add ambient flyby modules (#49)
    * Add module base prefab and scripts
    
    * Pass editable component instead of entity
    
    * Add ambient flyby module
    
    * Update stringtable
    
    * Rearrange module base methods and add init done bool
    
    * Make sure init done only runs on authority
    
    * Update meta file
    
    * Introduce module placing actions
    
    * Make module not streamable by default
    
    * Change default image for module
    
    * Add placing actions for opening editor browser
    
    * Require entity in world placing action
    
    * Sleep placing action
    
    * Spawn default occupants placing action
    
    * Force writing module attributes
    
    * Proper prefix for placing actions
    
    * Proper prefix modules
    
    * Prefix EditableModuleComponent
    
    * Rename placing actions to init actions
    
    * Add on placing canceled event
    
    * Fix localization and prefab prefixes
    
    * Fix editable component
    
    * Do not delete immediately
    
    * Update meta
    
    * Create separate config for module attributes
    
    * Add attributes
    
    * Add module type content browser filter
    
    * Add module type filter
    
    * Disable simulation for spawned assets until all init actions are done
    
    * Restore previous simulation state
    
    * Revise module activation
    
    * Revise init actions
    
    * Implement GME_VehicleHelper methods
    
    * Add exception handling to init actions
    
    * Prevent race condition between canceling and notification
    
    * Implement additional group helper methods
    
    * Add support for spawning helicopter crew
    
    * Implement method for getting faction from editable entity
    
    * Implement invoker for when a group's members spawned
    
    * Assign number to module type enum
    
    * Fix spelling
    
    * Use vehicle helper for spawning crew
    
    * Terminate simulation when rotor breaks
    
    * Add null checks
    
    * Fix spelling of formatting
    
    * Add missing comma
    
    * Fix missing braces
    
    * Delete helo when pilot is missing
    
    * Merge branch 'master' into feature/ambient-flyby

---
+++ addons/GME/Scripts/Game/GME/Components/GME_AmbientFlybyComponent.c
@@ -0,0 +1,188 @@
+//------------------------------------------------------------------------------------------------
+class GME_AmbientFlybyComponentClass : ScriptComponentClass
+{
+}
+
+//------------------------------------------------------------------------------------------------
+class GME_AmbientFlybyComponent : ScriptComponent
+{
+	[Attribute(defvalue: "30", desc: "Speed in m/s")]
+	protected float m_fSpeed;
+	
+	[Attribute(defvalue: "6000", desc: "Distance from starting position for triggering despawn in meters")]
+	protected float m_fDespawnDistance;
+	
+	protected Physics m_Physics;
+	protected vector m_vVelocity;
+	protected vector m_vStartPos;
+	protected ref array<AIAgent> m_aAgents = {};
+	protected SCR_CharacterControllerComponent m_pPilotController;
+	protected ref array<SCR_RotorDamageManagerComponent> m_aRotorDamageManagers = {};
+	
+	protected ref ScriptInvoker m_OnDespawn = new ScriptInvoker();
+	
+	//------------------------------------------------------------------------------------------------
+	override protected void OnPostInit(IEntity owner)
+	{
+		if (!GetGame().InPlayMode() || !Replication.IsServer())
+			return;
+
+		SetEventMask(owner, EntityEvent.SIMULATE);
+		m_Physics = owner.GetPhysics();
+		m_Physics.SetActive(ActiveState.ACTIVE);
+		
+		vector transform[4];
+		owner.GetWorldTransform(transform);
+		m_vStartPos = transform[3];
+		m_vVelocity = m_fSpeed * transform[2];
+		
+		GetGame().GetCallqueue().Call(DelayedInit, owner);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	protected void DelayedInit(IEntity owner)
+	{
+		SCR_AIGroup crewGroup = GME_VehicleHelper.SpawnCrew(Vehicle.Cast(owner));
+		crewGroup.GME_GetOnDoneSpawningMembers().Insert(OnCrewSpawned);
+		
+		HelicopterControllerComponent ctrl = HelicopterControllerComponent.Cast(owner.FindComponent(HelicopterControllerComponent));
+		if (ctrl)
+			ctrl.StartEngine();
+		
+		IEntity child = owner.GetChildren();
+		while (child)
+		{
+			SCR_RotorDamageManagerComponent damageManager = SCR_RotorDamageManagerComponent.Cast(child.FindComponent(SCR_RotorDamageManagerComponent));
+			if (damageManager)
+			{
+				damageManager.GetOnDamageStateChanged().Insert(OnRotorDamageStateChanged);
+				m_aRotorDamageManagers.Insert(damageManager);
+			}
+			
+			child = child.GetSibling();
+		}
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	protected void OnCrewSpawned(SCR_AIGroup group)
+	{
+		group.GME_GetOnDoneSpawningMembers().Remove(OnCrewSpawned);
+		group.GetAgents(m_aAgents);
+		m_aAgents.Insert(group);
+		
+		foreach (AIAgent agent : m_aAgents)
+		{
+			agent.SetPermanentLOD(0);
+		}
+		
+		SCR_ChimeraCharacter pilot = SCR_ChimeraCharacter.Cast(m_aAgents[0].GetControlledEntity());
+		if (!pilot)
+			return SCR_EntityHelper.DeleteEntityAndChildren(GetOwner());
+		
+		m_pPilotController = SCR_CharacterControllerComponent.Cast(pilot.FindComponent(SCR_CharacterControllerComponent));
+		if (!m_pPilotController)
+			return SCR_EntityHelper.DeleteEntityAndChildren(GetOwner());
+		
+		m_pPilotController.m_OnLifeStateChanged.Insert(OnPilotLifeStateChanged);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	override protected void EOnSimulate(IEntity owner, float timeSlice)
+	{
+		m_Physics.SetVelocity(m_vVelocity);
+		HandleDespawning(owner, timeSlice);
+	}
+	
+	protected const float DISTANCE_CHECK_TIMEOUT = 2;
+	protected float m_fDistanceCheckTimer = 0;
+	
+	//------------------------------------------------------------------------------------------------
+	//! Despawns owner when it moved the despawn distance
+	protected void HandleDespawning(IEntity owner, float timeSlice)
+	{
+		m_fDistanceCheckTimer += timeSlice;
+		if (m_fDistanceCheckTimer < m_fDistanceCheckTimer)
+			return;
+		
+		m_fDistanceCheckTimer = 0;
+		
+		if (vector.DistanceXZ(m_vStartPos, owner.GetOrigin()) < m_fDespawnDistance)
+			return;
+		
+		SCR_EntityHelper.DeleteEntityAndChildren(owner);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	//! Terminate simulation when the life state of the pilot changes
+	protected void OnPilotLifeStateChanged(ECharacterLifeState previousLifeState, ECharacterLifeState newLifeState)
+	{
+		TerminateSimulation();
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	protected void OnRotorDamageStateChanged(EDamageState state)
+	{
+		if (state != EDamageState.DESTROYED)
+			return;
+		
+		TerminateSimulation();
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	protected void TerminateSimulation()
+	{
+		foreach (AIAgent agent : m_aAgents)
+		{
+			if (agent)
+				agent.SetPermanentLOD(-1);
+		}
+		
+		ClearEventMask(GetOwner(), EntityEvent.SIMULATE);
+		HelicopterControllerComponent ctrl = HelicopterControllerComponent.Cast(GetOwner().FindComponent(HelicopterControllerComponent));
+		ctrl.StopEngine();
+		
+		if (m_pPilotController)
+			m_pPilotController.m_OnLifeStateChanged.Remove(OnPilotLifeStateChanged);
+		
+		foreach (SCR_RotorDamageManagerComponent damageManager : m_aRotorDamageManagers)
+		{
+			damageManager.GetOnDamageStateChanged().Remove(OnRotorDamageStateChanged);
+		}
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	void SetSpeed(float speed)
+	{
+		m_fSpeed = speed;
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	void SetDespawnDistance(float distance)
+	{
+		m_fDespawnDistance = distance;
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	void SetDirection(vector direction)
+	{
+		m_vVelocity = m_fSpeed * direction;
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	void SetStartPos(vector pos)
+	{
+		m_vStartPos = pos;
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	vector GetStartPos()
+	{
+		return m_vStartPos;
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	ScriptInvoker GetOnDespawn()
+	{
+		return m_OnDespawn;
+	}
+}
