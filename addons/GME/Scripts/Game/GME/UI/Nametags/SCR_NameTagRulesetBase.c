b71e3d98396a08b6312a325c536a00448e595048
Kex <kexanone@gmail.com>
2025-05-01 16:16:25 +0200

    Implement visibility getter in name tag data

--- addons/GME/Scripts/Game/GME/UI/Nametags/SCR_NameTagRulesetBase.c
+++ addons/GME/Scripts/Game/GME/UI/Nametags/SCR_NameTagRulesetBase.c
@@ -1,14 +1,15 @@
+//------------------------------------------------------------------------------------------------
 [BaseContainerProps()]
 modded class SCR_NameTagRulesetBase : Managed
 {
+	//------------------------------------------------------------------------------------------------
 	override bool TestVisibility(SCR_NameTagData data, float timeSlice)
 	{		
 		// test if entity are invisible
-		bool visible = GME_VisibilityHelper.GetVisibility(data.m_Entity);
-		if (!visible)
+		if (!data.GME_IsVisible())
 			return false;
 		
 		// otherwise we call super and return it, so it decides when nameplates are visible
 		return super.TestVisibility(data, timeSlice);	
 	}
-};
+}
