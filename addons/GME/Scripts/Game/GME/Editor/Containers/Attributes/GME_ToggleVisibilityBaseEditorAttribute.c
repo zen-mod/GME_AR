bd73df621dfa218ff863e333fd47bc65439c2c1e
Kex <kexanone@gmail.com>
2025-05-01 16:17:43 +0200

    Move common parts in visibility editor attribute to a base class

--- addons/GME/Scripts/Game/GME/Editor/Containers/Attributes/GME_ToggleVisibilityBaseEditorAttribute.c
+++ addons/GME/Scripts/Game/GME/Editor/Containers/Attributes/GME_ToggleVisibilityBaseEditorAttribute.c
@@ -1,26 +1,28 @@
 //------------------------------------------------------------------------------------------------
-//! Entity Attribute for toggling vehicle visiblity
+//! Entity Attribute for toggling visiblity
 [BaseContainerProps(), SCR_BaseEditorAttributeCustomTitle()]
-class GME_ToggleVisibilityVehicleEditorAttribute : GME_BaseToggleVisibilityEditorAttribute
+class GME_BaseToggleVisibilityEditorAttribute : SCR_ValidTypeBaseEditorAttribute
 {
-	
 	//------------------------------------------------------------------------------------------------
 	override SCR_BaseEditorAttributeVar ReadVariable(Managed item, SCR_AttributesManagerEditorComponent manager)
 	{
-		SCR_EditableVehicleComponent editableEntity = SCR_EditableVehicleComponent.Cast(item);
-		if (!editableEntity) 
+		SCR_EditableEntityComponent editableEntity = SCR_EditableEntityComponent.Cast(item);
+		if (!editableEntity)
 			return null;
 		
-		return super.ReadVariable(item, manager);
+		return SCR_BaseEditorAttributeVar.CreateBool(editableEntity.GME_IsVisible());
 	}
 	
 	//------------------------------------------------------------------------------------------------
 	override void WriteVariable(Managed item, SCR_BaseEditorAttributeVar var, SCR_AttributesManagerEditorComponent manager, int playerID)
 	{
-		SCR_EditableVehicleComponent editableEntity = SCR_EditableVehicleComponent.Cast(item);
+		if (!var)
+			return;
+		
+		SCR_EditableEntityComponent editableEntity = SCR_EditableEntityComponent.Cast(item);
 		if (!editableEntity)
 			return;
 		
-		super.WriteVariable(item, var, manager, playerID);
+		editableEntity.GME_SetIsVisible(var.GetBool());
 	}
 }
