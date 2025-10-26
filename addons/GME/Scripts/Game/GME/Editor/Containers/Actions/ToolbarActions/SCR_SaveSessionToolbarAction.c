7b63ecafd6bcb067acf1c6d9bc0abaf5ab5d3bdb
Kex <kexanone@gmail.com>
2025-10-26 16:09:54 +0100

    Use new persistence system for buttons

--- addons/GME/Scripts/Game/GME/Editor/Containers/Actions/ToolbarActions/SCR_SaveSessionToolbarAction.c
+++ addons/GME/Scripts/Game/GME/Editor/Containers/Actions/ToolbarActions/SCR_SaveSessionToolbarAction.c
@@ -1,16 +1,10 @@
 [BaseContainerProps(), SCR_BaseContainerCustomTitleUIInfo("m_Info")]
 modded class SCR_SaveSessionToolbarAction : SCR_EditorToolbarAction
 {
-	//---------------------------------------------------------------------------------------------
-	//! Enable saving for clients on PC (disabled on Xbox, since broken)
+	//------------------------------------------------------------------------------------------------
+	//! Enable saving for clients
 	override bool CanBeShown(SCR_EditableEntityComponent hoveredEntity, notnull set<SCR_EditableEntityComponent> selectedEntities, vector cursorWorldPosition, int flags)
 	{
-		//--- Disallow on client if not PC, or in MP for "Save" version ("Save As" is allowed in MP)
-		if ((!Replication.IsServer() && System.GetPlatform() != EPlatform.WINDOWS) || (!m_bSaveAs && Replication.IsRunning()))
-			return false;
-		
-		//--- Disallow if editor save struct is not configured
-		SCR_SaveLoadComponent saveLoadComponent = SCR_SaveLoadComponent.GetInstance();
-		return saveLoadComponent && saveLoadComponent.ContainsStruct(SCR_EditorStruct);
+		return GetGame().GetSaveGameManager().IsSavingAllowed();
 	}
-};
+}
