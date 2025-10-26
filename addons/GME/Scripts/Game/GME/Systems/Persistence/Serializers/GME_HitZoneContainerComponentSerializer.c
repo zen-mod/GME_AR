f32be5a6450b64ed7c134a8ff033b466d92608ca
Kex <kexanone@gmail.com>
2025-10-26 22:55:01 +0100

    Improve handling of derived serializers

--- addons/GME/Scripts/Game/GME/Systems/Persistence/Serializers/GME_HitZoneContainerComponentSerializer.c
+++ addons/GME/Scripts/Game/GME/Systems/Persistence/Serializers/GME_HitZoneContainerComponentSerializer.c
@@ -5,26 +5,25 @@ class GME_HitZoneContainerComponentSerializer : HitZoneContainerComponentSeriali
 	override protected ESerializeResult Serialize(notnull IEntity owner, notnull GenericComponent component, notnull BaseSerializationSaveContext context)
 	{
 		ESerializeResult result = super.Serialize(owner, component, context);
-		
-		SCR_DamageManagerComponent damageManager = SCR_DamageManagerComponent.Cast(component);
-		if (result == ESerializeResult.DEFAULT && damageManager.IsDamageHandlingEnabled())
+		if (result == ESerializeResult.ERROR)
 			return result;
 		
-		context.WriteValue("version", 1);
-		context.WriteValue("damageHandlingEnabled", damageManager.IsDamageHandlingEnabled());
-		return ESerializeResult.OK;
+		SCR_DamageManagerComponent damageManager = SCR_DamageManagerComponent.Cast(component);
+		if (!damageManager.IsDamageHandlingEnabled())
+		{
+			context.WriteValue("damageHandlingEnabled", false);
+			result = ESerializeResult.OK;
+		}
+		
+		return result;
 	}
 	
 	//------------------------------------------------------------------------------------------------
 	override protected bool Deserialize(notnull IEntity owner, notnull GenericComponent component, notnull BaseSerializationLoadContext context)
 	{
 		super.Deserialize(owner, component, context);
-		
 		SCR_DamageManagerComponent damageManager = SCR_DamageManagerComponent.Cast(component);
 		
-		int version;
-		context.Read(version);
-
 		bool damageHandlingEnabled;
 		if (context.Read(damageHandlingEnabled))
 			damageManager.EnableDamageHandling(false);
--- addons/GME/Scripts/Game/GME/Systems/Persistence/Serializers/SCR_EditableEntityComponentSerializer.c
+++ addons/GME/Scripts/Game/GME/Systems/Persistence/Serializers/SCR_EditableEntityComponentSerializer.c
@@ -5,28 +5,29 @@ modded class SCR_EditableEntityComponentSerializer : ScriptedComponentSerializer
 	override protected ESerializeResult Serialize(notnull IEntity owner, notnull GenericComponent component, notnull BaseSerializationSaveContext context)
 	{
 		ESerializeResult result = super.Serialize(owner, component, context);
-		
-		SCR_EditableEntityComponent editable = SCR_EditableEntityComponent.Cast(component);
-		if (result == ESerializeResult.DEFAULT && editable.GME_IsVisible())
+		if (result == ESerializeResult.ERROR)
 			return result;
 		
-		context.WriteValue("version", 1);
-		context.WriteValue("visible", editable.GME_IsVisible());
-		return ESerializeResult.OK;
+		SCR_EditableEntityComponent editable = SCR_EditableEntityComponent.Cast(component);
+		if (!editable.GME_IsVisible())
+		{
+			context.WriteValue("visible", false);
+			result = ESerializeResult.OK;
+		}
+		
+		return result;
 	}
 	
 	//------------------------------------------------------------------------------------------------
 	override protected bool Deserialize(notnull IEntity owner, notnull GenericComponent component, notnull BaseSerializationLoadContext context)
 	{
-		if (!super.Deserialize(owner, component, context))
-			return false;
-		
+		super.Deserialize(owner, component, context);
 		SCR_EditableEntityComponent editable = SCR_EditableEntityComponent.Cast(component);
-		int version;
-		context.Read(version);
+		
 		bool visible;
-		context.Read(visible);
-		editable.GME_SetIsVisible(visible);
+		if (context.Read(visible))
+			editable.GME_SetIsVisible(visible);
+		
 		return true;
 	}
 }
