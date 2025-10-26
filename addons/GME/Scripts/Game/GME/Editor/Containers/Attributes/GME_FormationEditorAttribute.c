b1379a4e9db7b18bff1f7d88d1da8f24ecbb4275
Kex <kexanone@gmail.com>
2025-10-26 21:53:32 +0100

    Use SCR_EditableGroupComponent and SCR_EAIGroupFormation for formation attribute

--- addons/GME/Scripts/Game/GME/Editor/Containers/Attributes/GME_FormationEditorAttribute.c
+++ addons/GME/Scripts/Game/GME/Editor/Containers/Attributes/GME_FormationEditorAttribute.c
@@ -1,49 +1,48 @@
-/**
-Entity Formation Attribute for getting and setting varriables in Editor Attribute window
-*/
+//------------------------------------------------------------------------------------------------
+//! Entity Formation Attribute for getting and setting varriables in Editor Attribute window
 [BaseContainerProps(), SCR_BaseEditorAttributeCustomTitle()]
 class GME_FormationEditorAttribute : SCR_BasePresetsEditorAttribute
 {
-	ref const array<string> m_formationNames = {"Wedge", "Column", "StaggeredColumn", "Line"};
-	
+	//------------------------------------------------------------------------------------------------
 	override SCR_BaseEditorAttributeVar ReadVariable(Managed item, SCR_AttributesManagerEditorComponent manager)
 	{
-		SCR_EditableEntityComponent editableEntity = SCR_EditableEntityComponent.Cast(item);
-		if (!editableEntity)
+		SCR_EditableGroupComponent editableGroup = SCR_EditableGroupComponent.Cast(item);
+		if (!editableGroup)
 			return null;
 		
-		IEntity owner =  editableEntity.GetOwner();
-		if (!owner)
+		AIGroup group =  AIGroup.Cast(editableGroup.GetOwner());
+		if (!group)
 			return null;
 		
-		AIFormationComponent formationComp = AIFormationComponent.Cast(owner.FindComponent(AIFormationComponent));
+		AIFormationComponent formationComp = AIFormationComponent.Cast(group.FindComponent(AIFormationComponent));
 		if (!formationComp)
 			return null;
 		
-		int formationIdx = m_formationNames.Find(formationComp.GetFormation().GetName());
-		if (formationIdx < 0)
+		int formationID = typename.StringToEnum(SCR_EAIGroupFormation, formationComp.GetFormation().GetName());
+		if (formationID < 0)
 			return null;
 		
-		return SCR_BaseEditorAttributeVar.CreateInt(formationIdx);
+		return SCR_BaseEditorAttributeVar.CreateInt(formationID);
 	}
 	
+	//------------------------------------------------------------------------------------------------
 	override void WriteVariable(Managed item, SCR_BaseEditorAttributeVar var, SCR_AttributesManagerEditorComponent manager, int playerID)
 	{
 		if (!var)
 			return;
 		
-		SCR_EditableEntityComponent editableEntity = SCR_EditableEntityComponent.Cast(item);
-		if (!editableEntity)
+		SCR_EditableGroupComponent editableGroup = SCR_EditableGroupComponent.Cast(item);
+		if (!editableGroup)
 			return;
 		
-		IEntity owner =  editableEntity.GetOwner();
-		if (!owner)
+		AIGroup group =  AIGroup.Cast(editableGroup.GetOwner());
+		if (!group)
 			return;
 		
-		AIFormationComponent formationComp = AIFormationComponent.Cast(owner.FindComponent(AIFormationComponent));
+		AIFormationComponent formationComp = AIFormationComponent.Cast(group.FindComponent(AIFormationComponent));
 		if (!formationComp)
 			return;
 		
-		formationComp.SetFormation(m_formationNames[var.GetInt()]);
+		formationComp.SetFormation(SCR_Enum.GetEnumName(SCR_EAIGroupFormation, var.GetInt()));
 	}
-};
+}
