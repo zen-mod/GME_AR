1c684d54b00d92fad269a298516b9428d42fea3a
Kex <kexanone@gmail.com>
2026-08-24 18:34:16 +0200

    Switch to recommended binary-serializer-compatible pattern

--- addons/GME/Scripts/Game/GME/Systems/Persistence/Serializers/GME_IntelComponentSerializer.c
+++ addons/GME/Scripts/Game/GME/Systems/Persistence/Serializers/GME_IntelComponentSerializer.c
@@ -10,14 +10,16 @@ class GME_IntelComponentSerializer : ScriptedComponentSerializer
 	//------------------------------------------------------------------------------------------------
 	override protected ESerializeResult Serialize(notnull IEntity owner, notnull GenericComponent component, notnull SaveContext context)
 	{
-
-		GME_IntelComponent intel = GME_IntelComponent.Cast(component);
+		const GME_IntelComponent intel = GME_IntelComponent.Cast(component);
 		if (!intel.HasIntel())
 			return ESerializeResult.DEFAULT;
 		
+		const string title = intel.GetTitle();
+		const string content = intel.GetContent();
+		
 		context.WriteValue("version", 1);
-		context.WriteValue("title", intel.GetTitle());
-		context.WriteValue("content", intel.GetContent());
+		context.Write(title);
+		context.Write(content);
 		return ESerializeResult.OK;
 	}
 	
@@ -25,14 +27,18 @@ class GME_IntelComponentSerializer : ScriptedComponentSerializer
 	override protected bool Deserialize(notnull IEntity owner, notnull GenericComponent component, notnull LoadContext context)
 	{
 		GME_IntelComponent intel = GME_IntelComponent.Cast(component);
+		
 		int version;
 		context.Read(version);
+		
 		string title;
 		context.Read(title);
 		intel.SetTitle(title);
+		
 		string content;
 		context.Read(content);
 		intel.SetContent(content);
+		
 		return true;
 	}
 }
--- addons/GME/Scripts/Game/GME/Systems/Persistence/Serializers/SCR_EditableEntityComponentSerializer.c
+++ addons/GME/Scripts/Game/GME/Systems/Persistence/Serializers/SCR_EditableEntityComponentSerializer.c
@@ -4,25 +4,45 @@ modded class SCR_EditableEntityComponentSerializer : ScriptedComponentSerializer
 	//------------------------------------------------------------------------------------------------
 	override protected ESerializeResult Serialize(notnull IEntity owner, notnull GenericComponent component, notnull SaveContext context)
 	{
-		ESerializeResult result = super.Serialize(owner, component, context);
-		if (result == ESerializeResult.ERROR)
-			return result;
+		const SCR_EditableEntityComponent editable = SCR_EditableEntityComponent.Cast(component);
+
+        context.StartObject("base");
+		const ESerializeResult baseResult = super.Serialize(owner, component, context);
+        context.EndObject();
 		
-		SCR_EditableEntityComponent editable = SCR_EditableEntityComponent.Cast(component);
-		if (!editable.GME_IsVisible())
+		if (baseResult == ESerializeResult.ERROR)
+			return ESerializeResult.ERROR;
+		
+		const bool visible = editable.GME_IsVisible();
+		
+		if (baseResult == ESerializeResult.DEFAULT &&
+			visible)
 		{
-			context.WriteValue("visible", false);
-			result = ESerializeResult.OK;
+			return ESerializeResult.DEFAULT;
 		}
 		
-		return result;
+		context.WriteValue("version", 1);
+		context.WriteDefault(visible, true);
+		return ESerializeResult.OK;
 	}
 	
 	//------------------------------------------------------------------------------------------------
 	override protected bool Deserialize(notnull IEntity owner, notnull GenericComponent component, notnull LoadContext context)
 	{
-		super.Deserialize(owner, component, context);
 		SCR_EditableEntityComponent editable = SCR_EditableEntityComponent.Cast(component);
+		
+		if (context.DoesObjectExist("base"))
+		{
+			if (!context.StartObject("base") ||
+				!super.Deserialize(owner, component, context) ||
+				!context.EndObject())
+			{
+				return false;
+			}
+		}
+		
+		int version;
+		context.Read(version);
 		
 		bool visible;
 		if (context.Read(visible))
